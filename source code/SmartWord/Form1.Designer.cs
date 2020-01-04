cb816853c213948aa98233ae63df2226512c3ba0
maratkaSharp <59148903+maratkaSharp@users.noreply.github.com>
2020-01-05 02:26:20 +0300

    Smart Word

---
+++ source code/SmartWord/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SmartWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel = new System.Windows.Forms.Panel();
+            this.button_search = new System.Windows.Forms.Button();
+            this.search = new System.Windows.Forms.TextBox();
+            this.button_save = new System.Windows.Forms.Button();
+            this.button_add = new System.Windows.Forms.Button();
+            this.timer_for_search = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel.Controls.Add(this.label1);
+            this.panel.Controls.Add(this.button_search);
+            this.panel.Controls.Add(this.search);
+            this.panel.Controls.Add(this.button_save);
+            this.panel.Controls.Add(this.button_add);
+            this.panel.Location = new System.Drawing.Point(20, 0);
+            this.panel.Margin = new System.Windows.Forms.Padding(4);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(722, 68);
+            this.panel.TabIndex = 1;
+            // 
+            // button_search
+            // 
+            this.button_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_search.Location = new System.Drawing.Point(683, 18);
+            this.button_search.Name = "button_search";
+            this.button_search.Size = new System.Drawing.Size(32, 32);
+            this.button_search.TabIndex = 4;
+            this.button_search.Text = "X";
+            this.button_search.UseVisualStyleBackColor = true;
+            this.button_search.Click += new System.EventHandler(this.Button_Search_Click);
+            // 
+            // search
+            // 
+            this.search.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.13F);
+            this.search.Location = new System.Drawing.Point(325, 18);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(360, 30);
+            this.search.TabIndex = 3;
+            this.search.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.search_KeyPress);
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(150, 18);
+            this.button_save.Margin = new System.Windows.Forms.Padding(4);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(100, 32);
+            this.button_save.TabIndex = 1;
+            this.button_save.Text = "Save";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.Button_save_Click);
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(10, 18);
+            this.button_add.Margin = new System.Windows.Forms.Padding(4);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(100, 32);
+            this.button_add.TabIndex = 1;
+            this.button_add.Text = "Add";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // timer_for_search
+            // 
+            this.timer_for_search.Interval = 1000;
+            this.timer_for_search.Tick += new System.EventHandler(this.timer_for_search_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "search";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 790);
+            this.Controls.Add(this.panel);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(781, 12296);
+            this.MinimumSize = new System.Drawing.Size(781, 47);
+            this.Name = "Form1";
+            this.Text = "Smart Word";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.TextBox search;
+        private System.Windows.Forms.Button button_search;
+        private System.Windows.Forms.Timer timer_for_search;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ source code/SmartWord/Form1.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SmartWord.Properties;
+using static System.Array;
+
+namespace SmartWord
+{
+    public partial class Form1 : Form
+    {
+        List<TextBox> smartWords          = new List<TextBox>();
+        List<RichTextBox> smartWordsValue = new List<RichTextBox>();
+        List<Panel> panels                = new List<Panel>();
+        List<Panel> yellowPanels          = new List<Panel>();
+        List<Panel> YPs                   = new List<Panel>(); // YPs - yellow panels
+        List<Graphics> grs                = new List<Graphics>();
+        List<Button> buttons              = new List<Button>();
+        List<string> Values               = new List<string>();
+        List<List<dynamic>> saveElems     = new List<List<dynamic>>(); // elems - elements
+
+        string searchText;
+
+        int actualIndexForSW = 1; // SW - Smart Words // thanks captain
+        int randNum          = 0;
+        int maxPW            = 0; // PW - Panel Width
+        Random random        = new Random();
+
+        bool isGrowing       = true;
+        bool TFPA            = false; // timer for panel anim
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            maxPW = Width - 55;
+
+            string words = Convert.ToString(Settings.Default["words"]);
+            string values = Convert.ToString(Settings.Default["values"]);
+            string text = "";
+            string symbolsCombination = "";
+
+            int symbolsCombinationCount = 0;
+
+            foreach (char elV in values) {
+                if ((elV == '/' && symbolsCombination == "") ||
+                    (elV == '&' && symbolsCombination == "/") ||
+                    (elV == '/' && symbolsCombination == "/&"))
+                {
+                    symbolsCombination += Convert.ToString(elV);
+                    if (symbolsCombination == "/&/") {
+                        symbolsCombination = "";
+                        Values.Add(text);
+                        text = "";
+                    }
+                } else { text += elV; }
+            }
+
+            foreach (char elW in words) {
+                if ((elW == '/' && symbolsCombination == "") || 
+                    (elW == '&' && symbolsCombination == "/") || 
+                    (elW == '/' && symbolsCombination == "/&")) {
+
+                    symbolsCombination += Convert.ToString(elW);
+                    if (symbolsCombination == "/&/") {
+                        symbolsCombination = "";
+                        CreateNewWord(text, Values[symbolsCombinationCount]);
+                        symbolsCombinationCount++;
+                        text = "";
+                    }
+                } else {
+                    text += elW;
+                }
+            }
+        }
+
+        void CreateNewWord(string textForTB, string textForRTB)
+        {
+                     Button button_delete        = new Button();
+                      TextBox tb                = new TextBox();
+                       RichTextBox rtb         = new RichTextBox();
+                        Panel newPanel        = new Panel();
+                         Panel yellowPanel   = new Panel();
+
+                         button_delete.Width = 46;
+                        button_delete.Height  = 40;
+                       tb.Width                = 150;
+                      tb.Height                 = 22;
+                     rtb.Width                   = 320;
+                    rtb.Height                    = 40;
+                   newPanel.Width                  = Width - 49;
+                  newPanel.Height                   = 50;
+                 yellowPanel.Width                   = 0;
+                yellowPanel.Height                    = 50;
+               tb.Text                                 = textForTB;
+              rtb.Text                                  = textForRTB;
+             button_delete.Text                          = "Delete";
+            yellowPanel.BackColor                         = Color.FromArgb(255, 217, 25);
+
+            if (randNum == 1) randNum = random.Next(2, 4);
+            else if (randNum == 2) { randNum = random.Next(1, 3); if (randNum == 2) randNum = 3; }
+            else if (randNum == 3) randNum = random.Next(1, 3);
+            else randNum = random.Next(1, 4);
+
+            if (randNum == 1) newPanel.BackColor = Color.DarkGray;
+            else if (randNum == 2) newPanel.BackColor = Color.Gray;
+            else if (randNum == 3) newPanel.BackColor = Color.LightGray;
+
+            if (actualIndexForSW == 1) {
+                tb.Location            = new Point(20, 90 * actualIndexForSW);
+                rtb.Location           = new Point(10 + tb.Width + tb.Location.X, 90 * actualIndexForSW - 10);
+                button_delete.Location = new Point(rtb.Location.X + rtb.Width + 5, 90 * actualIndexForSW - 10);
+                newPanel.Location      = new Point(15, 90 * actualIndexForSW - 15);
+                yellowPanel.Location   = new Point(15, 90 * actualIndexForSW - 15);
+            } else {
+                tb.Location            = new Point(20, 55 * actualIndexForSW + 35);
+                newPanel.Location      = new Point(15, 55 * actualIndexForSW + 20);
+                yellowPanel.Location   = new Point(15, 55 * actualIndexForSW + 20);
+                rtb.Location           = new Point(10 + tb.Width + tb.Location.X, 55 * actualIndexForSW + 25);
+                button_delete.Location = new Point(rtb.Location.X + rtb.Width + 5, 55 * actualIndexForSW + 25);
+            }
+
+            smartWords.Add(tb);
+            smartWordsValue.Add(rtb);
+            panels.Add(newPanel);
+            yellowPanels.Add(yellowPanel);
+            buttons.Add(button_delete);
+
+            button_delete.Name = Convert.ToString(actualIndexForSW);
+            button_delete.Click += Button_delete_Click;
+
+            this.Controls.Add(button_delete);
+            this.Controls.Add(tb);
+            this.Controls.Add(rtb);
+            this.Controls.Add(yellowPanel);
+            this.Controls.Add(newPanel);
+
+            actualIndexForSW++;
+
+            if (55 * actualIndexForSW > this.Height - 30) {
+                AutoScroll = true;
+            }
+        }
+
+        string letterTranslation(string arg)
+        {
+            string result = "";
+
+            foreach (char el in arg)
+            {
+                switch (el)
+                {
+                    case 'q': result += "й"; break;
+                    case 'w': result += "ц"; break;
+                    case 'e': result += "у"; break;
+                    case 'r': result += "к"; break;
+                    case 't': result += "е"; break;
+                    case 'y': result += "н"; break;
+                    case 'u': result += "г"; break;
+                    case 'i': result += "ш"; break;
+                    case 'o': result += "щ"; break;
+                    case 'p': result += "з"; break;
+                    case '[': result += "х"; break;
+                    case ']': result += "ъ"; break;
+                    case '{': result += "х"; break;
+                    case '}': result += "ъ"; break;
+                    case 'a': result += "ф"; break;
+                    case 's': result += "ы"; break;
+                    case 'd': result += "в"; break;
+                    case 'f': result += "а"; break;
+                    case 'g': result += "п"; break;
+                    case 'h': result += "р"; break;
+                    case 'j': result += "о"; break;
+                    case 'k': result += "л"; break;
+                    case 'l': result += "д"; break;
+                    case ';': result += "ж"; break;
+                    case (char)39: result += "э"; break;
+                    case ':': result += "ж"; break;
+                    case '"': result += "э"; break;
+                    case 'z': result += "я"; break;
+                    case 'x': result += "ч"; break;
+                    case 'c': result += "с"; break;
+                    case 'v': result += "м"; break;
+                    case 'b': result += "и"; break;
+                    case 'n': result += "т"; break;
+                    case 'm': result += "ь"; break;
+                    case '.': result += "ю"; break;
+                    case '>': result += "ю"; break;
+                    case ',': result += "б"; break;
+                    case '<': result += "б"; break;
+                    case 'й': result += "q"; break;
+                    case 'ц': result += "w"; break;
+                    case 'у': result += "e"; break;
+                    case 'к': result += "r"; break;
+                    case 'е': result += "t"; break;
+                    case 'н': result += "y"; break;
+                    case 'г': result += "u"; break;
+                    case 'ш': result += "i"; break;
+                    case 'щ': result += "o"; break;
+                    case 'з': result += "p"; break;
+                    case 'х': result += "["; break;
+                    case 'ъ': result += "]"; break;
+                    case 'Ф': result += "a"; break;
+                    case 'ы': result += "s"; break;
+                    case 'в': result += "d"; break;
+                    case 'а': result += "f"; break;
+                    case 'п': result += "g"; break;
+                    case 'р': result += "h"; break;
+                    case 'о': result += "j"; break;
+                    case 'л': result += "k"; break;
+                    case 'д': result += "l"; break;
+                    case 'ж': result += ";"; break;
+                    case 'э': result += "'"; break;
+                    case 'я': result += "z"; break;
+                    case 'ч': result += "x"; break;
+                    case 'с': result += "c"; break;
+                    case 'м': result += "v"; break;
+                    case 'и': result += "b"; break;
+                    case 'т': result += "n"; break;
+                    case 'ь': result += "m"; break;
+                    case 'б': result += ","; break;
+                    case 'ю': result += "."; break;
+                    default: result += el; break;
+                }
+            }
+
+            return result;
+        }
+
+        string MakeWordsSmaller(string arg) {
+            string result = "";
+
+            foreach (char el in arg) {
+                switch (el) {
+                    case 'Q': result += "q"; break;
+                    case 'W': result += "w"; break;
+                    case 'E': result += "e"; break;
+                    case 'R': result += "r"; break;
+                    case 'T': result += "t"; break;
+                    case 'Y': result += "y"; break;
+                    case 'U': result += "u"; break;
+                    case 'I': result += "i"; break;
+                    case 'O': result += "o"; break;
+                    case 'P': result += "p"; break;
+                    case 'A': result += "a"; break;
+                    case 'S': result += "s"; break;
+                    case 'D': result += "d"; break;
+                    case 'F': result += "f"; break;
+                    case 'G': result += "g"; break;
+                    case 'H': result += "h"; break;
+                    case 'J': result += "j"; break;
+                    case 'K': result += "k"; break;
+                    case 'L': result += "l"; break;
+                    case 'Z': result += "z"; break;
+                    case 'X': result += "x"; break;
+                    case 'C': result += "c"; break;
+                    case 'V': result += "v"; break;
+                    case 'B': result += "b"; break;
+                    case 'N': result += "n"; break;
+                    case 'M': result += "m"; break;
+                    case 'Й': result += "й"; break;
+                    case 'Ц': result += "ц"; break;
+                    case 'У': result += "у"; break;
+                    case 'К': result += "к"; break;
+                    case 'Е': result += "е"; break;
+                    case 'Н': result += "н"; break;
+                    case 'Г': result += "г"; break;
+                    case 'Ш': result += "ш"; break;
+                    case 'Щ': result += "щ"; break;
+                    case 'З': result += "з"; break;
+                    case 'Х': result += "х"; break;
+                    case 'Ъ': result += "ъ"; break;
+                    case 'Ф': result += "ф"; break;
+                    case 'Ы': result += "ы"; break;
+                    case 'В': result += "в"; break;
+                    case 'А': result += "а"; break;
+                    case 'П': result += "п"; break;
+                    case 'Р': result += "р"; break;
+                    case 'О': result += "о"; break;
+                    case 'Л': result += "л"; break;
+                    case 'Д': result += "д"; break;
+                    case 'Ж': result += "ж"; break;
+                    case 'Э': result += "э"; break;
+                    case 'Я': result += "я"; break;
+                    case 'Ч': result += "ч"; break;
+                    case 'С': result += "с"; break;
+                    case 'М': result += "м"; break;
+                    case 'И': result += "и"; break;
+                    case 'Т': result += "т"; break;
+                    case 'Ь': result += "ь"; break;
+                    case 'Б': result += "б"; break;
+                    case 'Ю': result += "ю"; break;
+                    default : result += el; break;
+                }
+            }
+
+            return result;
+        }
+
+        void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            CreateNewWord(null, null);
+        }
+
+        void Button_save_Click(object sender, EventArgs e)
+        {
+            int indexForSE = -1; // SE - save elems // elems - elements
+
+            Settings.Default["words"] = "";
+            Settings.Default["values"] = "";
+
+            for (int i = 0; i < smartWords.Count; i++) {
+                if (smartWords[i].Text.Trim() != "" || smartWordsValue[i].Text.Trim() != "") {
+                    indexForSE++;
+                    saveElems.Add(new List<dynamic>());
+                    saveElems[indexForSE].Add(smartWords[i].Text);
+                    saveElems[indexForSE].Add(smartWordsValue[i].Text);
+                }
+            }
+
+            for (int i = 0; i < saveElems.Count; i++) {
+                Settings.Default["words"] += Convert.ToString(saveElems[i][0]) + "/&/";
+                Settings.Default["values"] += Convert.ToString(saveElems[i][1]) + "/&/";
+            }
+
+            Settings.Default.Save();
+        }
+
+        void Button_Search_Click(object sender, EventArgs e)
+        {
+            search.Text = "";
+            foreach (Panel panel in YPs) { panel.Width = 0; }
+            YPs = new List<Panel>();
+        }
+
+        void timer_for_search_Tick(object sender, EventArgs e)
+        {
+            string str;
+            if (search.Text != "") {
+                searchText = MakeWordsSmaller(search.Text);
+                for (int i = 0; i < smartWords.Count; i++) {
+                    if (MakeWordsSmaller(smartWordsValue[i].Text).Contains(searchText)) {
+                        YPs.Add(yellowPanels[i]);
+                        yellowPanels[i].Width = panels[i].Width;
+                    } else if (letterTranslation(MakeWordsSmaller(smartWordsValue[i].Text)).Contains(searchText)) {
+                        YPs.Add(yellowPanels[i]);
+                        yellowPanels[i].Width = panels[i].Width;
+                    } else if (MakeWordsSmaller(smartWords[i].Text).Contains(searchText)) {
+                        YPs.Add(yellowPanels[i]);
+                        yellowPanels[i].Width = panels[i].Width;
+                    } else if (letterTranslation(MakeWordsSmaller(smartWords[i].Text)).Contains(searchText)) {
+                        YPs.Add(yellowPanels[i]);
+                        yellowPanels[i].Width = panels[i].Width;
+                    } else {
+                        YPs.Remove(yellowPanels[i]);
+                        yellowPanels[i].Width = 0;
+                    }
+                }
+            } else {
+                foreach (Panel panel in YPs) { panel.Width = 0; }
+                YPs = new List<Panel>();
+            }
+        }
+
+        void search_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (search.Text != "" || search.Text != " ") {
+                timer_for_search.Enabled = true;
+            } else {
+                timer_for_search.Enabled = false;
+                foreach (Panel panel in YPs) {
+                    panel.Width = 0;
+                    isGrowing = true;
+                }
+                YPs = new List<Panel>();
+            }
+        }
+
+        void Button_delete_Click(object sender, EventArgs e) {
+            int btnID = Convert.ToInt32((sender as Button).Name);
+            if (btnID == actualIndexForSW) {
+                smartWords[btnID-1].Dispose();
+                smartWordsValue[btnID-1].Dispose();
+                panels[btnID-1].Dispose();
+                yellowPanels[btnID-1].Dispose();
+                buttons[btnID-1].Dispose();
+
+                smartWords.RemoveAt(btnID-1);
+                smartWordsValue.RemoveAt(btnID-1);
+                panels.RemoveAt(btnID-1);
+                yellowPanels.RemoveAt(btnID-1);
+                buttons.RemoveAt(btnID-1);
+            } else {
+                smartWords[btnID-1].Dispose();
+                smartWordsValue[btnID-1].Dispose();
+                panels[btnID-1].Dispose();
+                yellowPanels[btnID-1].Dispose();
+                buttons[btnID-1].Dispose();
+
+                smartWords.RemoveAt(btnID-1);
+                smartWordsValue.RemoveAt(btnID-1);
+                panels.RemoveAt(btnID-1);
+                yellowPanels.RemoveAt(btnID-1);
+                buttons.RemoveAt(btnID-1);
+
+                for (int i = btnID-1; i < panels.Count; i++) {
+                    smartWords[i].Location = new Point(smartWords[i].Location.X, smartWords[i].Location.Y - 55);
+                    smartWordsValue[i].Location = new Point(smartWordsValue[i].Location.X, smartWordsValue[i].Location.Y - 55);
+                    panels[i].Location = new Point(panels[i].Location.X, panels[i].Location.Y - 55);
+                    yellowPanels[i].Location = new Point(yellowPanels[i].Location.X, yellowPanels[i].Location.Y - 55);
+                    buttons[i].Location = new Point(buttons[i].Location.X, buttons[i].Location.Y - 55);
+                    buttons[i].Name = Convert.ToString(Convert.ToInt32(buttons[i].Name) - 1);
+                }
+            }
+
+            actualIndexForSW--;
+        }
+    }
+}
